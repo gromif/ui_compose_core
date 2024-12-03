a37664a7e6e51f933a593310fd80742a2ed8f683
Nevidimka655 <alex.kobrys@gmail.com>
2024-12-03 11:49:42 +0200

    Remove nestedScroll

--- src/main/java/com/nevidimka655/ui/compose_core/Preferences.kt
+++ src/main/java/com/nevidimka655/ui/compose_core/Preferences.kt
@@ -17,9 +17,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalView
-import androidx.compose.ui.platform.rememberNestedScrollInteropConnection
 import androidx.compose.ui.semantics.Role
 import com.nevidimka655.haptic.Haptic
 import com.nevidimka655.haptic.hapticToggleOff
